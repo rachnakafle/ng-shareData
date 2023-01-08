f3b840a02d1c654458a5a70cb1d4d0eec1422d2f
rachnakafle <rachanakafle99@gmail.com>
2023-01-08 16:42:41 +0545

    Sharing data betn unrelated components

---
+++ src/app/covid-data.ts
@@ -0,0 +1,5 @@
+export class CovidData {
+  state!: string ;
+  confirmed!: number;
+  delta!: number;
+}
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,51 @@
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { ModalService } from './modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent implements OnInit {
+  @Input() id!: string;
+  private element: any;
+
+  constructor(private modalService: ModalService, private el: ElementRef) {
+    this.element = el.nativeElement;
+  }
+
+  ngOnInit(): void {
+    if (!this.id) {
+      console.error('modal must have an id');
+      return;
+    }
+
+    document.body.appendChild(this.element);
+
+    this.element.addEventListener(
+      'click',
+      (el: { target: { className: string } }) => {
+        if (el.target.className === 'my-modal') {
+          this.close();
+        }
+      }
+    );
+
+    this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
+
+  open(): void {
+    this.element.style.display = 'block';
+    document.body.classList.add('my-modal-open');
+  }
+
+  close(): void {
+    this.element.style.display = 'none';
+    document.body.classList.remove('my-modal-open');
+  }
+}
